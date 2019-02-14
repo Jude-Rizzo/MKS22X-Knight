feb19af513ac6fffba6cbc1594bd10d957eb305d
Jude-Rizzo <jprizzo2020@gmail.com>
2019-02-14 11:38:42 -0500

    horray

--- KnightBoard.java
+++ KnightBoard.java
@@ -10,27 +10,43 @@ public class KnightBoard{
     for(int i = 0; i < board.length; i++){
       ans += "\n";
       for(int j = 0; j < board.length; j++){
-          if(board[i][j]<10)ans += "_"+board[i][j] + "  ";
-          else ans+=board[i][j] + "  ";
-        }
-      }return ans;
-    }
+        if(board[i][j]<10)ans += "_"+board[i][j] + "  ";
+        else ans+=board[i][j] + "  ";
+      }
+    }return ans;
+  }
 
 
   private boolean solveH(int row, int col, int level){
     //level is the move number of the knight
-    if(level == board.length * board[0].length - 1) return true;
-   if(row < 0 || col< 0) return false;
-   if(row >= board.length || col >= board[0].length)return false;
-   if(board[row][col] > 0) return false;
+    if(level == board.length * board[0].length - 1){
 
-   //make sure the move is valid, then move the knight there and continue
-   board[row][col] = level;
-    return (solveH(row + 2, col + 1, level + 1)||solveH(row + 2, col - 1, level + 1)
+      return true;
+    }
+    if(row < 0 || col< 0){
+      return false;
+    }
+
+    if(row >= board.length || col >= board[0].length)
+    return false;
+
+    if(board[row][col] > 0)
+    return false;
+
+    //make sure the move is valid, then move the knight there and continue
+    board[row][col] = level;
+    boolean ans = (solveH(row + 2, col + 1, level + 1)||solveH(row + 2, col - 1, level + 1)
+
     || solveH(row - 2, col + 1, level + 1)||solveH(row - 2, col - 1, level + 1) ||
+
     solveH(row + 1, col + 2, level + 1)||solveH(row + 1, col - 2, level + 1)
+
     || solveH(row - 1, col + 2, level + 1)||solveH(row - 1, col - 2, level + 1));
     //^^ trying all the possible knight moves
+    if(!ans)
+    board[row][col] = 0;
+
+    return ans;
 
   }
 
@@ -40,7 +56,7 @@ public class KnightBoard{
   }
 
   public static void main(String[] args){
-    KnightBoard k = new KnightBoard(8,8);
+    KnightBoard k = new KnightBoard(5,5);
     k.solve(0,0);
     System.out.println(k);
   }
