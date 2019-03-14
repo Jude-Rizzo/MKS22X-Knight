23fc3851e9d85697c396421b7e0e3a3b5c75d7d5
Jude Rizzo <jrizzo00@stuy.edu>
2019-03-13 23:09:23 -0400

    knightsTour

--- Tester.java
+++ Tester.java
@@ -33,7 +33,8 @@ public static void runTest(int i){
 public static void main(String[] args){
   runTest(4);
   KnightBoard b = new KnightBoard(16, 16);
-  System.out.println(b.solve(0,0));
+  b.solve(0,0);
+  System.out.println(b);
 }
 
 }
